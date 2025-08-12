127ee9c0846f500983dbfea4b97be26691aeb11e
Gokul M R <gokulmr331@gmail.com>
2025-08-12 11:03:06 +0530

    feature:upload middleware created

--- backend/src/routes/fileRoutes.ts
+++ backend/src/routes/fileRoutes.ts
@@ -1,13 +1,15 @@
 import express, { Router } from "express"
 import { deleteFile, getFile, getFiles, uplaodFile } from "../controllers/FileController"
+import { upload } from "../middlewares/upload"
 
 
 const fileRouter = express.Router()
 
-fileRouter.post('/upload',uplaodFile)
+fileRouter.post('/upload',upload.single("file"),uplaodFile)
 fileRouter.post('/get',getFile)
 fileRouter.post('/get-files',getFiles)
 fileRouter.post('/delete',deleteFile)
 
 
 export default fileRouter
+
